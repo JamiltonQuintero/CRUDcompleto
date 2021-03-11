e8bd007403e5831d251b4559529ee6a99ad416b3
JamiltonQUintero <transportador1118@gmail.com>
2021-03-11 06:57:12 -0500

    Se agraga la clase Empleado

---
+++ Models/Entities/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using Castle.MicroKernel.SubSystems.Conversion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CRUDcompleto.Models.Entities
+{
+    public class Empleado
+    {
+        [Key]
+        public int EmpleadoId{ get; set; }
+
+        [Column("NombreEmpleado", TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+        public int Documento { get; set; } 
+        public int CargoId { get; set; }
+        public string Telefono { get; set; }
+
+
+
+    }
+}
